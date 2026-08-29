c323d0eac2a9d0230de2aa2ad286e0323d6ffeb1
andromeda951 <andromeda951@gmail.com>
2026-08-29 08:33:01 +0700

    update

---
+++ loop.py
@@ -0,0 +1,55 @@
+# for i in range(5):
+#     print("hello")
+
+# i=0   print hello
+# i=1   print hello
+# i=2   print hello
+# i=3   print hello
+# i=4   print hello
+# i=5
+
+
+# for i in range(5):
+#     print(i)
+
+# i=0   print 0
+# i=1   print 1
+# i=2   print 2
+# i=3   print 3
+# i=4   print 4
+
+
+# 0+1+2+3+4=10
+# total = 0
+# for i in range(5):
+#     total = total + i
+# print(total)
+# print(total/5)
+# i=0   total=0+0=0
+# i=1   total=0+1=1
+# i=2   total=1+2=3
+# i=3   total=3+3=6
+# i=4   total=6+4=10
+
+
+# total = 0
+# for i in range(5):
+#     x = int(input())
+#     total = total + x
+# print("total =", total)
+# i=0   x=2     total=0+2=2
+# i=1   x=5     total=2+5=7
+# i=2   x=3     total=7+3=10
+# i=3   x=100     total=10+100=110
+# i=4   x=9     total=110+9=119
+
+
+# range(end)
+# range(start, end)
+# range(start, end, step)
+
+for i in range(3, 10, 2):
+    print(i)
+
+# lanjut cara kedua
+    
